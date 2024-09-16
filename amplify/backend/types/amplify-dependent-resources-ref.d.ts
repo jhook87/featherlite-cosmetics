d65c1e59af72481f380c33a4ab7a392bc613341e
jhook87 <justinhooker87@gmail.com>
2024-09-16 08:08:59 -0400

    schema work

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Trigger01713da7": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "featherlite": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
