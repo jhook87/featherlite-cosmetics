7e9c15888640b118a19187dce3608aa9cad5d005
jhook87 <justinhooker87@gmail.com>
2025-09-03 20:24:12 -0700

    Featherlite-storefront

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#D6D1D1',
+        accent: '#3DA212',
+        background: '#FFFFFF',
+        text: '#000000',
+      },
+      fontFamily: {
+        heading: ['\'Crimson Pro\'', 'serif'],
+        body: ['\'Josefin Sans\'', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
